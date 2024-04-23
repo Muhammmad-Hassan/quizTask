84296bd46be07c0f04b3bf739dece8a620fa9d97
Class04 <hassankhan5uy99@gmail.com>
2024-04-23 16:40:07 +0500

    third commit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-document.getElementById("studentForm").addEventListener("submit", function (event) {
-  event.preventDefault();
-  var formData = ["rollNo", "name", "batch", "section"];
-  var valid = formData.every(function (field) {
-    var value = document.getElementById(field).value;
-    if (!value) {
+document
+  .getElementById("studentForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var rollNo = document.getElementById("rollNo").value;
+    var name = document.getElementById("name").value;
+    var batch = document.getElementById("batch").value;
+    var section = document.getElementById("section").value;
+
+    // Validate form data
+    if (!rollNo || !name || !batch || !section) {
       alert("Please fill all the fields.");
-      return false;
+      return;
     }
-    sessionStorage.setItem(field, value);
-    return true;
-  });
-  if (valid) {
+
+    // Store the form data in session storage
+    sessionStorage.setItem("rollNo", rollNo);
+    sessionStorage.setItem("name", name);
+    sessionStorage.setItem("batch", batch);
+    sessionStorage.setItem("section", section);
+
+    // Redirect to the quiz page
     window.location.href = "/quiz.html";
-  }
-});
+  });
