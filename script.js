619d042e9cabf3bbe3ab64985bd0c786a1f6c96e
Class04 <hassankhan5uy99@gmail.com>
2024-04-23 14:00:04 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("studentForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  var formData = ["rollNo", "name", "batch", "section"];
+  var valid = formData.every(function (field) {
+    var value = document.getElementById(field).value;
+    if (!value) {
+      alert("Please fill all the fields.");
+      return false;
+    }
+    sessionStorage.setItem(field, value);
+    return true;
+  });
+  if (valid) {
+    window.location.href = "/quiz-app-main/index.html";
+  }
+});
